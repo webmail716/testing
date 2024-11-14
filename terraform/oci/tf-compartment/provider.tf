850e3bfba7db40b3b5f3d38b8e33e59cd4e41e89
Jason Greschler <>
2024-11-14 11:10:18 -0500

    add files for OCI tutorial

---
+++ terraform/oci/tf-compartment/provider.tf
@@ -0,0 +1,9 @@
+provider "oci" {
+  private_key_path = "/Users/jgreschler/.oci/oci_api_key.pem"
+  private_key_password = "#JG#oci1"
+  
+  tenancy_ocid = "ocid1.tenancy.oc1..aaaaaaaattxfaekw56ayn3zehgzdofvsjbhde3tvoex56saqrkuy2eckc6cq"
+  user_ocid = "ocid1.user.oc1..aaaaaaaafoysdqg2zy32wehagp5u3c6ib7jagkpvcnr5xor5c6snuo35z5xq" 
+  fingerprint = "48:3b:d2:02:67:0d:49:bd:c5:e9:77:af:b0:0b:de:98"
+  region = "us-ashburn-1"
+}
