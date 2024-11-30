23c62bb5f5e20662af31aa26e86263acebd49701
Jason Greschler <>
2024-11-30 18:28:34 -0500

    refactor into resource specific files

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -12,38 +12,6 @@ provider "aws" {
 }
 
 
-# SSH Key Pair
-resource "aws_key_pair" "deployer" {
-  key_name   = "sysinfo-deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public key
-}
-
-# Security group
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Attach security group to instance
-# resource "aws_instance" "linux_vm" {
-#   # ... previous configuration ...
-#   vpc_security_group_ids =[aws_security_group.allow_ssh.id]
-# }
-
 # Linux Instance
 # Ubuntu Server 24.04 LTS (HVM), SSD Volume Type
 
@@ -52,20 +20,14 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_instance" "linux_vm" {
   ami           = "ami-0ea3c35c5c3284d82" # Ubuntu Server 24.04 LTS (HVM), SSD Volume Type
 
-  instance_type = "t2.micro" # Use a free tier eligible instance type
+  instance_type = "t3.micro" # Use a free tier eligible instance type
 
   key_name      = aws_key_pair.deployer.key_name
-
-#  user_data = <<-EOF
-#              #!/bin/bash
-#              yum update -y
-#              yum install -y ruby
-#              gem install /path/to/sysinfo-1.0.gem
-#              EOF
 
   tags = {
     Name = "SysInfoLinuxVM"
   }
 
+# Attach security group to instance
   vpc_security_group_ids =[aws_security_group.allow_ssh.id]
 }
