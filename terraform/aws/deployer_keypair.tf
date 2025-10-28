9a9919feee938c56acf318008f448c789d710458
Jason Greschler <jagreschler@gmail.com>
2025-10-28 13:54:40 -0400

    remove old project code

--- terraform/aws/deployer_keypair.tf
+++
@@ -1,6 +0,0 @@
-# SSH Key Pair
-resource "aws_key_pair" "deployer" {
-  key_name   = "sysinfo-deployer-key"
-  public_key = file("~/.ssh/id_rsa.pub")  # Path to your public key
-}
-
--- terraform/oci/tf-compartment/compartment.tf
+++
@@ -1,7 +0,0 @@
-
-resource "oci_identity_compartment" "tf-compartment" {
-    # Required
-    compartment_id = "ocid1.tenancy.oc1..aaaaaaaattxfaekw56ayn3zehgzdofvsjbhde3tvoex56saqrkuy2eckc6cq"
-    description = "Compartment for Terraform resources."
-    name = "my-compartment-name"
-}
